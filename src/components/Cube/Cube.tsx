d8823e2fda2008f4c496fb14850ede1ef0737b89
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 08:53:33 -0300

    feat: add cube animation

--- src/components/Cube/Cube.tsx
+++ src/components/Cube/Cube.tsx
@@ -1,13 +1,35 @@
-import { MeshProps } from '@react-three/fiber';
-import React from 'react';
+import { MeshProps, useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import {
+  BufferGeometry,
+  Material,
+  Mesh,
+  NormalBufferAttributes,
+  Object3DEventMap,
+} from 'three';
 
 type Props = {
   color: string;
 } & MeshProps;
 
 function Cube({ color, ...rest }: Props) {
+  const cubeMesh =
+    useRef<
+      Mesh<
+        BufferGeometry<NormalBufferAttributes>,
+        Material | Material[],
+        Object3DEventMap
+      >
+    >(null);
+
+  useFrame((_, delta) => {
+    cubeMesh.current.rotation.x += delta;
+    cubeMesh.current.rotation.y += delta;
+    cubeMesh.current.rotation.z += delta;
+  });
+
   return (
-    <mesh {...rest} scale={1}>
+    <mesh {...rest} scale={1} ref={cubeMesh}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={color} />
     </mesh>
