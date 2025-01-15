84e4e19767dcef0c5983c428a6b5ac8ac11244ba
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 08:53:47 -0300

    feat: add dodecahedron animation

--- src/components/Dodecahedron/Dodecahedron.tsx
+++ src/components/Dodecahedron/Dodecahedron.tsx
@@ -1,13 +1,35 @@
-import { MeshProps } from '@react-three/fiber';
-import React from 'react';
+import { MeshProps, useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import {
+  BufferGeometry,
+  Material,
+  Mesh,
+  NormalBufferAttributes,
+  Object3DEventMap,
+} from 'three';
 
 type Props = {
   color: string;
 } & MeshProps;
 
 function Dodecahedron({ color, ...rest }: Props) {
+  const dodecahedronMesh =
+    useRef<
+      Mesh<
+        BufferGeometry<NormalBufferAttributes>,
+        Material | Material[],
+        Object3DEventMap
+      >
+    >(null);
+
+  useFrame((_, delta) => {
+    dodecahedronMesh.current.rotation.x += delta;
+    dodecahedronMesh.current.rotation.y += delta;
+    dodecahedronMesh.current.rotation.z += delta;
+  });
+
   return (
-    <mesh {...rest} scale={1}>
+    <mesh {...rest} scale={1} ref={dodecahedronMesh}>
       <dodecahedronGeometry args={[0.7, 0]} />
       <meshStandardMaterial color={color} />
     </mesh>
