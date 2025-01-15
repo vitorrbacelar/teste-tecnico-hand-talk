c393723fb34f79595a85ae2adf9e68697f9529e6
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 03:31:51 -0300

    chore: passing down dodecahedron mesh props

--- src/components/Dodecahedron/Dodecahedron.tsx
+++ src/components/Dodecahedron/Dodecahedron.tsx
@@ -1,12 +1,13 @@
+import { MeshProps } from '@react-three/fiber';
 import React from 'react';
 
 type Props = {
   color: string;
-};
+} & MeshProps;
 
-function Dodecahedron({ color }: Props) {
+function Dodecahedron({ color, ...rest }: Props) {
   return (
-    <mesh>
+    <mesh {...rest} scale={1}>
       <dodecahedronGeometry args={[0.7, 0]} />
       <meshStandardMaterial color={color} />
     </mesh>
