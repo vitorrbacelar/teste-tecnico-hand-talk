4cdfbea0d2d0a4665b73c2f9af9141e03eee98af
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 20:18:17 -0300

    chore: added realtime database

--- src/configs/firebase/firebaseConfig.ts
+++ src/configs/firebase/firebaseConfig.ts
@@ -5,6 +5,7 @@ import {
   API_KEY,
   APP_ID,
   AUTH_DOMAIN,
+  DATABASE_URL,
   MEASUREMENT_ID,
   MESSAGING_SENDER_ID,
   PROJECT_ID,
@@ -14,6 +15,7 @@ import {
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
