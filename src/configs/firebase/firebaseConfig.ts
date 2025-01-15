5edd57b96bf5e764447ee3c92b29c017518d7fea
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 10:38:10 -0300

    refactor: pass firebase keys by .env

--- src/configs/firebase/firebaseConfig.ts
+++ src/configs/firebase/firebaseConfig.ts
@@ -1,15 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDs0uw7HoRfCdkuVk6IuWRXKm0eb9BbNpw',
-  authDomain: 'teste-hand-talk.firebaseapp.com',
-  projectId: 'teste-hand-talk',
-  storageBucket: 'teste-hand-talk.firebasestorage.app',
-  messagingSenderId: '347408030921',
-  appId: '1:347408030921:web:096356cb0aaa2dc378c118',
-  measurementId: 'G-4L09RK1E88',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
