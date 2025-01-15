c89a4ca6486e9578f0f0900fb6593e68a4425cf2
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 03:45:27 -0300

    feat: home screen

---
+++ src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,27 @@
+import { SafeAreaView, useWindowDimensions } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Canvas } from '@react-three/fiber';
+import Dodecahedron from '../../components/Dodecahedron/Dodecahedron';
+import Cone from '../../components/Cone/Cone';
+import Cube from '../../components/Cube/Cube';
+import { styles } from './styles';
+
+export default function HomeScreen() {
+  const { height, width } = useWindowDimensions();
+
+  const canvasHeight = (75 / 100) * height;
+
+  return (
+    <SafeAreaView style={styles.homeContainer}>
+      <StatusBar style="light" backgroundColor="orange" />
+      <Canvas style={{ height: canvasHeight, width }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        <Cube position={[0, 2, 0]} color={'red'} />
+        <Cone position={[0, 0, 0]} color={'blue'} />
+        <Dodecahedron position={[0, -2, 0]} color={'yellow'} />
+      </Canvas>
+    </SafeAreaView>
+  );
+}
