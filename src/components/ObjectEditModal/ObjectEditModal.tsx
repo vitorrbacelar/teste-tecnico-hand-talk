654750467038c0c112530e004049e7552a4b48f8
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 18:54:02 -0300

    feat: ObjectEditModal component

---
+++ src/components/ObjectEditModal/ObjectEditModal.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { Picker } from '@react-native-picker/picker';
+import TextInputWithLabel from '../TextInputWithLabel/TextInputWithLabel';
+import { Object3D } from '../../modules/domain/entities/Object3D';
+import DefaultButton from '../Button/Button';
+import { styles } from './styles';
+
+type SubmitReturn = {
+  color: string;
+  shape: 'cube' | 'cone' | 'dodecahedron';
+  rotation: string;
+};
+
+type ObjectEditModalProps = {
+  onSave: (data: SubmitReturn) => void;
+  selectedObject: number;
+  defaultConfig: Object3D;
+};
+
+export default function ObjectEditModal({
+  onSave,
+  defaultConfig,
+}: ObjectEditModalProps) {
+  const { control, handleSubmit, setValue, setFocus } = useForm<SubmitReturn>({
+    defaultValues: {
+      color: defaultConfig.color,
+      shape: defaultConfig.shape,
+      rotation: String(defaultConfig.rotation[0]),
+    },
+  });
+
+  const onSubmit = (data: SubmitReturn) => {
+    onSave(data);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Tipo de Forma</Text>
+      <Controller
+        name="shape"
+        control={control}
+        render={({ field: { value } }) => (
+          <Picker
+            selectedValue={value}
+            onValueChange={itemValue => setValue('shape', itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Cubo" value="cube" />
+            <Picker.Item label="Cone" value="cone" />
+            <Picker.Item label="Dodecaedro" value="dodecahedron" />
+          </Picker>
+        )}
+      />
+
+      <Controller
+        name="color"
+        control={control}
+        render={({ field: { onChange, value, ref } }) => (
+          <TextInputWithLabel
+            ref={ref}
+            label="Cor"
+            style={styles.input}
+            value={value}
+            onChangeText={text => setValue('color', text)}
+            placeholder="Cor em hexadecimal (#RRGGBB)"
+            onChange={onChange}
+          />
+        )}
+      />
+
+      <Controller
+        name="rotation"
+        control={control}
+        render={({ field: { onChange, value, ref } }) => (
+          <TextInputWithLabel
+            ref={ref}
+            label="Rotação"
+            style={styles.input}
+            value={value}
+            onChangeText={text => setValue('rotation', text)}
+            placeholder="Rotação (em graus)"
+            keyboardType="numeric"
+            onChange={onChange}
+          />
+        )}
+      />
+
+      <DefaultButton
+        title="Salvar Configurações"
+        onPress={handleSubmit(onSubmit)}
+      />
+    </View>
+  );
+}
---
+++ src/components/ObjectEditModal/styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#ececec',
+    alignItems: 'center',
+  },
+  label: {
+    color: 'black',
+    paddingBottom: 2,
+    fontWeight: 600,
+    marginLeft: 5,
+    width: '80%',
+  },
+  picker: {
+    height: 50,
+    marginBottom: 16,
+    width: '80%',
+    borderColor: 'black',
+    borderRadius: 6,
+    borderWidth: 2,
+    backgroundColor: 'white',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#CCC',
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+    backgroundColor: 'white',
+  },
+});
