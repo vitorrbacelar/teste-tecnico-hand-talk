be92519e27f74f23b441bc97b3fda5156bc4868b
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 18:56:19 -0300

    chore: added the object config provider to the LoggedIn stack

--- src/routes/LoggedIn/LoggedInStack.tsx
+++ src/routes/LoggedIn/LoggedInStack.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/Home/HomeScreen';
 import ConfigScreen from '../../screens/ConfigScreen/ConfigScreen';
+import { ObjectConfigProvider } from '../../contexts/ObjectConfigContext';
 
 export type LoggedInStackParamList = {
   Home: undefined;
@@ -11,17 +12,19 @@ const Stack = createNativeStackNavigator<LoggedInStackParamList>();
 
 export default function LoggedInStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Config"
-        component={ConfigScreen}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
+    <ObjectConfigProvider>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Config"
+          component={ConfigScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </ObjectConfigProvider>
   );
 }
