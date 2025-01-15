3b185acce4ab9828e8a3851f09dce4dd08c557f8
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-15 08:53:15 -0300

    feat: add cone animation

--- src/components/Cone/Cone.tsx
+++ src/components/Cone/Cone.tsx
@@ -1,13 +1,35 @@
-import { MeshProps } from '@react-three/fiber';
-import React from 'react';
+import { MeshProps, useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import {
+  BufferGeometry,
+  Material,
+  Mesh,
+  NormalBufferAttributes,
+  Object3DEventMap,
+} from 'three';
 
 type Props = {
   color: string;
 } & MeshProps;
 
 function Cone({ color, ...rest }: Props) {
+  const coneMesh =
+    useRef<
+      Mesh<
+        BufferGeometry<NormalBufferAttributes>,
+        Material | Material[],
+        Object3DEventMap
+      >
+    >(null);
+
+  useFrame((_, delta) => {
+    coneMesh.current.rotation.x += delta;
+    coneMesh.current.rotation.y += delta;
+    coneMesh.current.rotation.z += delta;
+  });
+
   return (
-    <mesh {...rest} scale={1}>
+    <mesh {...rest} scale={1} ref={coneMesh}>
       <coneGeometry args={[0.6, 1, 30]} />
       <meshStandardMaterial color={color} />
     </mesh>
