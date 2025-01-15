3316acc1e89be6dc7ebea774c77a83a28ddbb42a
vitorrbacelar <vitorrbacelar@gmail.com>
2025-01-14 21:41:41 -0300

    chore: pass down Controller props and ref to internal input

--- src/components/TextInputWithLabel/TextInputWithLabel.tsx
+++ src/components/TextInputWithLabel/TextInputWithLabel.tsx
@@ -1,17 +1,27 @@
 import React, { Text, TextInput, TextInputProps, View } from 'react-native';
 import { styles } from './styles';
+import { forwardRef, useRef } from 'react';
 
 type Props = {
   label: string;
 } & TextInputProps;
 
-const TextInputWithLabel = ({ label }: Props) => {
-  return (
-    <View style={styles.textInputContainer}>
-      <Text style={styles.label}>{label}</Text>
-      <TextInput style={styles.textInput} />
-    </View>
-  );
-};
+const TextInputWithLabel = forwardRef(
+  ({ label, onChangeText, value, ...rest }: Props, ref) => {
+    const internalInputRef = useRef<TextInput>(null);
+    return (
+      <View style={styles.textInputContainer}>
+        <Text style={styles.label}>{label}</Text>
+        <TextInput
+          ref={internalInputRef}
+          style={styles.textInput}
+          value={value}
+          onChangeText={onChangeText}
+          {...rest}
+        />
+      </View>
+    );
+  },
+);
 
 export default TextInputWithLabel;
